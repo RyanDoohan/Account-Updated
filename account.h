4b9275510359c55abd44e8c52d32bf8c66922c22
Ryan <doohanryan@gmail.com>
2022-02-23 17:03:27 -0500

    Initial commit

---
+++ account.h
@@ -0,0 +1,17 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+
+#include <iostream>
+#include <string>
+
+class Account {
+    private:
+        std::string username, password;
+
+    public:
+        
+};
+
+
+
+#endif _ACCOUNT_H_
